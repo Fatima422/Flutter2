17b828829c7d8b63fe8d561a21a0e45efc481082
Fatima <108236976+Fatima422@users.noreply.github.com>
2023-01-05 04:25:21 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'Splash.dart';
+
+
+void main() {
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: SplashScreen(),
+    );
+  }
+}
